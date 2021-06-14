acab38b3517a9438a376dbca6fbcd72ae2406626
Ragurajan Thiruneelakandan <M1056164@mindtree.com>
2021-06-14 07:39:11 +0530

    Add project files.

---
+++ Controllers/EventController.cs
@@ -0,0 +1,109 @@
+﻿using Abstraction.Services;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet]
+        [Route("GetAllLocations")]
+        public async Task<ActionResult> Get()
+        {
+            ResponsePayload<EventLocation> response = new ResponsePayload<EventLocation>();
+           
+                try
+                {
+                    response.StatusCode = 200;
+                    response.Message = "success";
+                    response.DataList = await _eventService.GetAllLocations();
+                }
+                catch (Exception exception)
+                {
+                    response.StatusCode = 400;
+                    response.Message = "Can't get any location,please try again " + exception.Message;
+                }
+           
+            return Ok(response);
+
+        }
+
+        [HttpGet]
+        [Route("GetAllEventByLocation")]
+        public async Task<ActionResult> GetAllEventByLocation(int id)
+        {
+            ResponsePayload<EventDetails> response = new ResponsePayload<EventDetails>();
+
+            try
+            {
+                response.StatusCode = 200;
+                response.Message = "success";
+                response.DataList = await _eventService.GetAllEventByLocation(id);
+            }
+            catch (Exception exception)
+            {
+                response.StatusCode = 400;
+                response.Message = "Can't get any location,please try again " + exception.Message;
+            }
+
+            return Ok(response);
+
+        }
+        [HttpPost]
+        [Route("addBooking")]
+        public async Task<ActionResult> AddBooking([FromBody] EventBooking booking)
+        {
+            ResponsePayload<string> response = new ResponsePayload<string>();
+
+            try
+            {
+                response.StatusCode = 200;
+                response.AddedIdentity = await _eventService.AddBooking(booking);
+                response.Message = "Booking Details added successfully";
+                
+            }
+            catch (Exception exception)
+            {
+                response.StatusCode = 400;
+                response.Message = "Adding Booking details failed" + exception.Message;
+            }
+
+            return Ok(response);
+
+        }
+        [HttpPost]
+        [Route("updateBooking")]
+        public async Task<ActionResult> UpdateBooking([FromBody] EventBooking booking)
+        {
+            ResponsePayload<string> response = new ResponsePayload<string>();
+
+            try
+            {
+                response.StatusCode = 200;
+                response.Message = await _eventService.UpdateBooking(booking);
+
+            }
+            catch (Exception exception)
+            {
+                response.StatusCode = 400;
+                response.Message = "Adding Booking details failed" + exception.Message;
+            }
+
+            return Ok(response);
+
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Abstraction.Services;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TicketSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+       
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<ActionResult>  Post([FromBody] User user)
+        {
+            ResponsePayload<User> response = new ResponsePayload<User>();
+            User existinguser = await _userService.CheckIfUserIsPresent(user.UserEmail);
+            if (existinguser == null)
+            { 
+                 try
+                {
+                    response.StatusCode = 200;
+                    response.Message = await _userService.AddUserToDatabase(user);
+                }
+                catch (Exception exception)
+                {
+                    response.StatusCode = 400;
+                    response.Message = "User could not be added to database due to " + exception.Message;
+                }
+            }
+            else
+            {
+                response.StatusCode = 200;
+                response.DataList.Add(existinguser);
+                response.Message = "The user is already registered with the application";
+            }
+            return Ok(response);
+
+        }
+
+    }
+}
